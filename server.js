58f467e6cbd7fe92a97184e7a5200e5f7df109eb
Konstantin Kuznetcov <konstantin56k@gmail.com>
2021-06-13 16:19:45 -0400

    dealers_choice_sequelize done

--- server.js
+++ server.js
@@ -1,31 +1,52 @@
+const { conn, syncAndSeed, models: { Department, Employee} } = require('./db');
 const express = require('express');
-const {syncAndSeed, conn, models: {Department, Employee}} = require('./db');
 const app = express();
 
-app.get('/api/departments', async(req, res, next) => {
-    try {
-        res.send(await Department.findAll({
-            include: [{
-                model: Employee,
-                as: 'manager'
-            }]
-        })
-    )}
-    catch(error) {
-        next(error)
-    }
+app.get('/api/departments', async(req, res, next)=> {
+  try {
+    res.send(await Department.findAll({
+      include: [
+        {
+          model: Employee,
+          as: 'manager'
+        }
+      ]
+    }));
+  }
+  catch(ex){
+    next(ex);
+  }
 });
 
-const init = async() => {
-    try {
-        conn.authenticate();
-        await syncAndSeed();
-        const port = process.env.PORT || 3000;
-        app.listen(port, () => console.log(`listening on port ${port}`)) 
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
+app.get('/api/employees', async(req, res, next)=> {
+  try {
+    res.send(await Employee.findAll({
+      include: [
+        {
+          model: Employee,
+          as: 'supervisor'
+        },
+        Employee,
+        Department
+      ]
+    }));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+
+const init = async()=> {
+  try {
+    await conn.authenticate();
+    await syncAndSeed();
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
+  }
+  catch(ex){
+    console.log(ex);
+  }
+};
 
 init();
